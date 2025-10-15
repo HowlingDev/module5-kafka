a639b63f9267f88efcb4c3bfe077bd91fb48a3e3
GrigoriyVolk <grigoriyvolk228@gmail.com>
2025-10-15 17:26:34 +0300

    init commit

---
+++ src/main/java/com/example/Module5KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module5KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module5KafkaApplication.class, args);
+    }
+
+}
